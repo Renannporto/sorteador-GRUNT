535c4c6e08ceb31ab62625389dd93b551685989b
Renannporto <144285350+Renannporto@users.noreply.github.com>
2025-01-16 17:22:20 +0000

    tarefa

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,30 @@
-document.addEventListener("DOMcontentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("form-sorteador").addEventListener("submit", function(evento){
         evento.preventDefault();
         let numeroMaximo = document.getElementById("numero-maximo").value;
         numeroMaximo = parseInt(numeroMaximo);
 
-        let numeroAleatorio = Match.random() * numeroMaximo;
-        numeroAleatorio = Match.floor(numeroAleatorio + 1);
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
         document.getElementById("resultado-valor").innerText = numeroAleatorio;
         document.querySelector(".resultado").style.display = "block";
+
+        let params = {
+            particleCount: 500, // Quantidade de confetes
+            spread: 90, // O quanto eles se espalham
+            startVelocity: 70, // Velocidade inicial
+            origin: { x: 0, y: 0.5 }, // Posição inicial na tela
+            angle: 45 // Ângulo em que os confetes serão lançados
+        };
+    
+        // Joga confetes da esquerda pra direita
+        confetti(params);
+    
+        // Joga confetes da direita para a esquerda
+        params.origin.x = 1;
+        params.angle = 135;
+        confetti(params);
+        
     });
 });
